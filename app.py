3532a9bedaa8552ac69a34447802989b75da82a2
bclai7 <bclai27@gmail.com>
2018-02-22 19:23:24 -0500

    added login system and added "artist ranking" page

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from flask.ext.bcrypt import Bcrypt
+from functools import wraps
 import config as cf
 
 
@@ -21,14 +22,17 @@ mysql = MySQL(app)
 # init bcrypt for hashing
 bcrypt = Bcrypt(app)
 
+# Home Page
 @app.route('/')
 def index():
     return render_template('home.html')
 
+# Criteria Settings
 @app.route('/mycriteria')
 def mycriteria():
     return render_template('mycriteria.html')
 
+# Page for rating artists
 @app.route('/artistratings')
 def myratings():
     # Create MySQL Cursor
@@ -44,14 +48,17 @@ def myratings():
 
     return render_template('myratings.html', artistList=ArtistList)
 
+# Rankings page
+@app.route('/rankings')
+def rankings():
+    return render_template('rankings.html')
+
+# About page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
+# Registration Form
 class RegistrationForm(Form):
     name = StringField('Name', [validators.Length(min=4, max=25)])
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
@@ -61,6 +68,7 @@ class RegistrationForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
+# Registration Page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
@@ -83,8 +91,48 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
+# Login
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get form fields
+        email = request.form['email']
+        password_candidate = request.form['password']
 
+        # Create MySQL Cursor
+        cur = mysql.connection.cursor()
 
+        # Find user by email in database
+        result = cur.execute("SELECT * FROM user WHERE email=%s", [email])
+
+        if result > 0:
+            data = cur.fetchone()
+            password = data['password']
+            name = data['name']
+
+            # Compare entered password to hash value
+            if (bcrypt.check_password_hash(password, password_candidate)):
+                session['logged_in']=True
+                session['email']=email
+                session['name']=name
+
+                flash('Successfully Logged In', 'success')
+                return redirect(url_for('rankings'))
+            else:
+                return render_template('login.html', error = 'Email and password combination is incorrect')
+            # Close connection
+            cur.close()
+        else:
+            return render_template('login.html', error = 'No user found with that email address')
+    return render_template('login.html')
+
+# Logout
+@app.route('/logout')
+def logout():
+    if 'logged_in' in session:
+        session.clear()
+        flash('You have been logged out', 'success')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.secret_key=cf.SECRET_KEY
